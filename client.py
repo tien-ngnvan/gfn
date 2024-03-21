d46f8893dfe2ff68518ea087d29b062bee8e29cc
hieupth <64821726+hieupth@users.noreply.github.com>
2024-03-21 20:24:24 +0700

    chore: test websocket

---
+++ client.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+import websocket
+import websockets
+import sys
+import pickle
+import struct ### new code
+import asyncio
+
+# def on_message(ws, message):
+#     print(f"Received message: {message}")
+
+# def on_error(ws, error):
+#     print(f"Encountered error: {error}")
+
+# def on_close(ws, close_status_code, close_msg):
+#     print("Connection closed")
+
+# def on_open(ws):
+#     print("Connection opened")
+#     ws.send("Hello, Server!")
+
+# if __name__ == "__main__":
+#     ws = websocket.WebSocketApp("ws://127.0.0.1:8000/ws",
+#                                 on_message=on_message,
+#                                 on_error=on_error,
+#                                 on_close=on_close)
+#     ws.on_open = on_open
+#     ws.run_forever()
+
+async def hello(uri):
+    async with websockets.connect(uri) as websocket:
+        cap = cv2.VideoCapture(0)
+        while True:
+            # mess = 'hello'
+            # await websocket.send(mess.encode())
+            # greeting = await websocket.recv()
+            # print(f"Received: {greeting}")
+            ret, frame = cap.read()
+            data = pickle.dumps(frame)
+            await websocket.send(data)
+            greeting = await websocket.recv()
+            print(f"Received: {greeting}")
+
+asyncio.run(hello('ws://127.0.0.1:8000/ws'))
--- gfn/api.py
+++ gfn/api.py
@@ -1,12 +1,14 @@
 import cv2
 import asyncio
 import numpy as np
+import pickle
+import logging
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 
 app = FastAPI()
 
-@app.websocket('/')
+@app.websocket('/ws')
 async def socket(websocket: WebSocket):
   """
   Websocket endpoint that will be sending request to from the frontend.
@@ -21,12 +23,11 @@ async def socket(websocket: WebSocket):
     task.cancel()
     await websocket.close()
 
-
 async def socket_receive(websocket: WebSocket, queue: asyncio.Queue):
   """
   Asynchronous function that will be used to receive websocket connections from frontend.
   """
-  print("X")
+  logging.info("Receive")
   bytes = await websocket.receive_bytes()
   try:
     queue.put_nowait(bytes)
@@ -37,7 +38,7 @@ async def socket_receive(websocket: WebSocket, queue: asyncio.Queue):
 async def kyc(websocket: WebSocket, queue: asyncio.Queue):
   while True:
     bytes = await queue.get()
-    data = np.frombuffer(bytes, dtype=np.unit8)
-    image = cv2.imdecode(data, 1)
-    result = dict()
-    await websocket.send_json(result)
+    data = pickle.loads(bytes)
+    data = cv2.resize(data, (640, 640))
+    data = pickle.dumps(data)
+    await websocket.send_text("receive")
