a7885c89b8f5b811ce5532f6db9ab1a550de0f39
hieupth <64821726+hieupth@users.noreply.github.com>
2024-03-24 00:04:41 +0700

    chore: add base64

--- gfn/utils/image.py
+++ gfn/utils/image.py
@@ -1,5 +1,8 @@
 import cv2
+from PIL import Image
 import numpy as np
+import base64
+from io import BytesIO
 
 
 def crops(image, bbox):
@@ -12,3 +15,13 @@ def crops(image, bbox):
   return image[y1:y2, x1:x2]
 
 
+def to_base64(image: Image) -> str:
+  buffered = BytesIO()
+  image.save(buffered, format="JPEG")
+  img_str = base64.b64encode(buffered.getvalue())
+  return img_str
+
+
+def from_base64(base64str: str) -> Image:
+  im = Image.open(BytesIO(base64.b64decode(base64str)))
+  return im
