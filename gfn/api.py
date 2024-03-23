5fc7a523c2093832625192c04590e73ef71d65a0
hieupth <64821726+hieupth@users.noreply.github.com>
2024-03-24 00:23:55 +0700

    chore: make upload API

--- gfn/api.py
+++ gfn/api.py
@@ -1,25 +1,95 @@
+import os
+import io
 import cv2
+import json
 import asyncio
 import numpy as np
-import pickle
-import logging
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from gfn.models import HeadFace, SpoofingNet
+from PIL import Image
+from sklearn.preprocessing import normalize 
+from fastapi import *
+from gfn.models import HeadFace, SpoofingNet, GhostFaceNet
+from gfn.utils import image as imgutils
 
 
-app = FastAPI()
-#
-headface = HeadFace('weights/yolov7-hf-v1.onnx')
-#
-spoofing = SpoofingNet('weights/OCI2M.onnx')
-spoofing_thresh = 0.7
+"""
+Load models and thresh.
+"""
+# For face storage.
+FACES = list()
+# For detection.
+DETECTION = HeadFace(os.getenv('DETECTION_MODEL_PATH', default='weights/yolov7-hf-v1.onnx'))
+DETECTION_THRESH = os.getenv('DETECTION_THRESH', default=0.5)
+# For anti-spoofing.
+SPOOFING = SpoofingNet(os.getenv('SPOOFING_MODEL_PATH', default='weights/OCI2M.onnx'))
+SPOOFING_THRESH = os.getenv('SPOOFING_THRESH', default=0.68)
+# For recognition.
+RECOGNITION = GhostFaceNet(os.getenv('RECOGNITION_MODEL_PATH', default='weights/ghostnetv1.onnx'))
+RECOGNITION_THRESH = os.getenv('RECOGNITION_THRESH', default=0.4)
 
-
+"""
+Major recognition functions.
+"""
 def softmax(x):
   return np.exp(x)/np.sum(np.exp(x))
 
+def compare_cosine(embed, anchor):
+    return np.dot(normalize(embed), anchor.T)[0]
 
-@app.websocket('/ws')
+def detect_face(img):
+  boxes, scores, _, kpts = DETECTION.detect(img, get_layer='face', conf_det=DETECTION_THRESH)
+  if len(boxes) == 1:
+    spoof = SPOOFING.get_features(img, boxes, kpts)
+    spoof = softmax(spoof)[:,0]
+    if spoof[0] > SPOOFING_THRESH:
+      embs = RECOGNITION.get_features(img, boxes, kpts)
+      return boxes[0], embs[0]
+    else:
+      return boxes[0], None
+  return None, None
+
+def recognize_face(img, emb):
+  result = compare_cosine(emb, FACES)
+  rec_idx = result.argmax(-1)
+  return result[rec_idx] > RECOGNITION_THRESH
+
+
+"""
+FastAPI
+"""
+app = FastAPI()
+
+"""
+For face register APIs
+"""
+IMAGE_FILE_TYPES = ["image/png", "image/jpeg", "image/jpg", "image/heic", "image/heif", "image/heics", "png", "jpeg", "jpg", "heic", "heif", "heics"]
+
+@app.post('/face')
+async def post_face_image(request: Request, file: UploadFile = File(...)):
+  request_object_content = await file.read()
+  try:
+    img = Image.open(io.BytesIO(request_object_content))
+    npimg = np.array(img)
+    face = detect_face(npimg)
+    if face[0] is not None and face[1] is not None:
+      face_item = {
+        'image': imgutils.to_base64(img),
+        'box': face[0],
+        'emb': face[1] 
+      }
+      FACES.append(face_item)
+    return {'Ok'}
+    pass
+  except:
+    raise HTTPException(status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE, detail="Unsupported file type") 
+  
+@app.get('/face')
+async def get_fae_image():
+  return 
+
+"""
+For websocket streaming inference.
+"""
+@app.websocket('/')
 async def socket(websocket: WebSocket):
   """
   Websocket endpoint that will be sending request to from the frontend.
@@ -38,30 +108,15 @@ async def socket_receive(websocket: WebSocket, queue: asyncio.Queue):
   """
   Asynchronous function that will be used to receive websocket connections from frontend.
   """
-  logging.info("Receive")
   bytes = await websocket.receive_bytes()
   try:
     queue.put_nowait(bytes)
   except asyncio.QueueFull:
     pass
 
-
 async def kyc(websocket: WebSocket, queue: asyncio.Queue):
   while True:
     bytes = await queue.get()
-    img = pickle.loads(bytes)
-    fimg = img.copy()
-    res = dict()
-    #
-    boxes, scores, _, kpts = headface.detect(img, get_layer='face', conf_det=0.5)
-    if len(boxes) == 1:
-      res['box'] = boxes
-      #
-      spoof = spoofing.get_features(fimg, boxes, kpts)
-      spoof = softmax(spoof)[:,0]
-      if spoof[0] > spoofing_thresh:
-        res['id'] = 'real'
-      else:
-        res['id'] = 'fake'
-    data = pickle.dumps(res)
     await websocket.send_bytes(data)
+
+
