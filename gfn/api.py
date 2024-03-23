d1ce5d545f4be513ca4e9079083bcfba971acc7a
hieupth <64821726+hieupth@users.noreply.github.com>
2024-03-24 01:27:29 +0700

    feat: upload APIs

--- gfn/api.py
+++ gfn/api.py
@@ -1,12 +1,14 @@
 import os
 import io
 import cv2
-import json
+import hashlib
+import pathlib
 import asyncio
 import numpy as np
 from PIL import Image
 from sklearn.preprocessing import normalize 
 from fastapi import *
+from fastapi.staticfiles import StaticFiles
 from gfn.models import HeadFace, SpoofingNet, GhostFaceNet
 from gfn.utils import image as imgutils
 
@@ -16,6 +18,9 @@ Load models and thresh.
 """
 # For face storage.
 FACES = list()
+FACES_IMG_DIR = os.getenv('IMG_DIR', default='face_images')
+FACES_IMG_DIR = pathlib.Path(FACES_IMG_DIR)
+FACES_IMG_DIR.mkdir(exist_ok=True)
 # For detection.
 DETECTION = HeadFace(os.getenv('DETECTION_MODEL_PATH', default='weights/yolov7-hf-v1.onnx'))
 DETECTION_THRESH = os.getenv('DETECTION_THRESH', default=0.5)
@@ -47,44 +52,56 @@ def detect_face(img):
       return boxes[0], None
   return None, None
 
-def recognize_face(img, emb):
+def recognize_face(emb):
   result = compare_cosine(emb, FACES)
   rec_idx = result.argmax(-1)
   return result[rec_idx] > RECOGNITION_THRESH
 
+def build_emb():
+  for images in os.listdir(str(FACES_IMG_DIR)):
+    # check if the image ends with png
+    if (images.endswith(".jpg")):
+      name, ext = os.path.splitext(images)
+      path = FACES_IMG_DIR.joinpath(images)
+      image = Image.open(path)
+      box, emb = detect_face(np.array(image))
+      if emb is not None:
+        FACES.append(emb)
+      else:
+        os.remove(path)
+build_emb()
 
 """
 FastAPI
 """
 app = FastAPI()
+app.mount(f'/imgs', StaticFiles(directory=str(FACES_IMG_DIR)), name='images')
 
 """
 For face register APIs
 """
-IMAGE_FILE_TYPES = ["image/png", "image/jpeg", "image/jpg", "image/heic", "image/heif", "image/heics", "png", "jpeg", "jpg", "heic", "heif", "heics"]
-
-@app.post('/face')
+@app.post('/register')
 async def post_face_image(request: Request, file: UploadFile = File(...)):
   request_object_content = await file.read()
   try:
     img = Image.open(io.BytesIO(request_object_content))
-    npimg = np.array(img)
-    face = detect_face(npimg)
-    if face[0] is not None and face[1] is not None:
-      face_item = {
-        'image': imgutils.to_base64(img),
-        'box': face[0],
-        'emb': face[1] 
-      }
-      FACES.append(face_item)
-    return {'Ok'}
-    pass
+    box, emb = detect_face(np.array(img))
   except:
     raise HTTPException(status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE, detail="Unsupported file type") 
+  if emb is not None:
+    hash = hashlib.md5(img.tobytes()).hexdigest()
+    FACES.append(emb)
+    img.save(str(FACES_IMG_DIR.joinpath(f'{hash}.jpg')))
+    return {'image': f'/imgs/{hash}.jpg'}
+  elif box is None:
+    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='No face founded')
+  else:
+    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Maybe fake image')
   
-@app.get('/face')
+@app.get('/faces')
 async def get_fae_image():
-  return 
+  res = [f'/imgs/{k}' for k in os.listdir(str(FACES_IMG_DIR)) if k.endswith(".jpg")]
+  return res
 
 """
 For websocket streaming inference.
@@ -108,7 +125,7 @@ async def socket_receive(websocket: WebSocket, queue: asyncio.Queue):
   """
   Asynchronous function that will be used to receive websocket connections from frontend.
   """
-  bytes = await websocket.receive_bytes()
+  bytes = await websocket.receive_text()
   try:
     queue.put_nowait(bytes)
   except asyncio.QueueFull:
@@ -116,7 +133,13 @@ async def socket_receive(websocket: WebSocket, queue: asyncio.Queue):
 
 async def kyc(websocket: WebSocket, queue: asyncio.Queue):
   while True:
-    bytes = await queue.get()
-    await websocket.send_bytes(data)
+    b64 = await queue.get()
+    img = imgutils.from_base64(b64)
+    box, emb = detect_face(np.array(img))
+    
+    res = {}
+    if box is not None:
+      pass
+    # await websocket.send_bytes(data)
 
 
